76b0b396e5a8e8e7b25f5ae429ccb410d941fd3b
Adam Scholey <hello@adamjs.net>
2020-10-09 22:19:43 +0100

    Add `ImagesAgent` method for parsing timestamp from the filename of a provided file + add tests

---
+++ service/domain/images_test.go
@@ -0,0 +1,104 @@
+package domain_test
+
+import (
+	"imgnheap/service/domain"
+	"imgnheap/service/models"
+	"testing"
+	"time"
+)
+
+func TestImagesAgent_ParseTimestampFromFile(t *testing.T) {
+	var imgAgent domain.ImagesAgent
+
+	loc, err := time.LoadLocation("UTC")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedTs := time.Date(2018, 5, 26, 14, 0, 29, 0, loc)
+
+	t.Run("parsing timestamp from file that has a compatible filename must provide timestamp parsed from filename", func(t *testing.T) {
+		testCases := []string{
+			"20180526140029",
+			"20180526_140029",
+			"20180526-140029",
+			"Screenshot_20180526140029",
+			"Screenshot_20180526_140029",
+			"Screenshot_20180526-140029",
+			"Screenshot_20180526140029_MyFaceSpace",
+			"Screenshot_20180526_140029_MyFaceSpace",
+			"Screenshot_20180526-140029_MyFaceSpace",
+			"Screenshot 2018-05-26 at 14.00.29",
+		}
+
+		for _, tc := range testCases {
+			file := models.File{Name: tc}
+			actualTs := imgAgent.ParseTimestampFromFile(file)
+
+			if !expectedTs.Equal(actualTs) {
+				t.Fatalf("expected %+v, got %+v", expectedTs, actualTs)
+			}
+		}
+	})
+
+	t.Run("parsing timestamp from file that does not have a compatible filename must provide file created at timestamp", func(t *testing.T) {
+		testCases := []string{
+			"hello-world",
+			"12345678",
+			"true",
+
+			// all garbage date strings!
+			"2006660102150405",
+			"2006011102150405",
+			"2006010222150405",
+			"2006010215550405",
+			"2006010215044405",
+			"2006010215040555",
+			"2006660102_150405",
+			"2006011102_150405",
+			"2006010222_150405",
+			"20060102_15550405",
+			"20060102_15044405",
+			"20060102_15040555",
+			"2006660102-150405",
+			"2006011102-150405",
+			"2006010222-150405",
+			"20060102-15550405",
+			"20060102-15044405",
+			"20060102-15040555",
+			"Screenshot_2006660102150405",
+			"Screenshot_2006011102150405",
+			"Screenshot_2006010222150405",
+			"Screenshot_2006010215550405",
+			"Screenshot_2006010215044405",
+			"Screenshot_2006010215040555",
+			"Screenshot_2006660102_150405",
+			"Screenshot_2006011102_150405",
+			"Screenshot_2006010222_150405",
+			"Screenshot_20060102_15550405",
+			"Screenshot_20060102_15044405",
+			"Screenshot_20060102_15040555",
+			"Screenshot_2006660102-150405",
+			"Screenshot_2006011102-150405",
+			"Screenshot_2006010222-150405",
+			"Screenshot_20060102-15550405",
+			"Screenshot_20060102-15044405",
+			"Screenshot_20060102-15040555",
+			"Screenshot 200666-01-02 at 15.04.05",
+			"Screenshot 2006-0111-02 at 15.04.05",
+			"Screenshot 2006-01-0222 at 15.04.05",
+			"Screenshot 2006-01-02 at 1555.04.05",
+			"Screenshot 2006-01-02 at 15.0444.05",
+			"Screenshot 2006-01-02 at 15.04.0555",
+		}
+
+		for _, tc := range testCases {
+			file := models.File{Name: tc, CreatedAt: expectedTs}
+			actualTs := imgAgent.ParseTimestampFromFile(file)
+
+			if !expectedTs.Equal(actualTs) {
+				t.Fatalf("expected %+v, got %+v", expectedTs, actualTs)
+			}
+		}
+	})
+}
--- service/models/models.go
+++ service/models/models.go
@@ -1,7 +1,10 @@
 package models
 
+import "time"
+
 // File represents a single file
 type File struct {
-	Name string
-	Ext  string
+	Name      string
+	Ext       string
+	CreatedAt time.Time
 }
