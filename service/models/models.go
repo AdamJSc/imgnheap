37486fd0ec28e8e442c68d9810aa68e42580ac32
Adam Scholey <hello@adamjs.net>
2020-10-13 08:11:14 +0100

    Move Session model to `models` source file

--- service/models/models.go
+++ service/models/models.go
@@ -1,6 +1,21 @@
 package models
 
-import "time"
+import (
+	"path"
+	"time"
+)
+
+// Session defines a basic key/value session
+type Session struct {
+	Token   string
+	BaseDir string
+	SubDir  string
+}
+
+// FullDir returns the full directory stored by the Session
+func (s *Session) FullDir() string {
+	return path.Join(s.BaseDir, s.SubDir)
+}
 
 // File represents a single file
 type File struct {
